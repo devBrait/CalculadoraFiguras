8da0893feba17eb2d5fec0d52e99c0bdf4e0dd99
Vinícius Brait <121905548+devBrait@users.noreply.github.com>
2023-01-06 18:59:32 -0300

    Add files via upload

---
+++ FigurasGeometricas/FigurasGeometricas/FormPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FigurasGeometricas
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuadrado_Click(object sender, EventArgs e)
+        {
+            AreaQuadrado area = new AreaQuadrado();
+            area.ShowDialog();
+        }
+
+        private void btnCirculo_Click(object sender, EventArgs e)
+        {
+            AreaCirculo area = new AreaCirculo();
+            area.ShowDialog();
+        }
+
+        private void btnTriangulo_Click(object sender, EventArgs e)
+        {
+            AreaTriangulo area = new AreaTriangulo();
+            area.ShowDialog();
+        }
+
+        private void btnRetangulo_Click(object sender, EventArgs e)
+        {
+            AreaRetangulo area = new AreaRetangulo();
+            area.ShowDialog();
+        }
+    }
+}
